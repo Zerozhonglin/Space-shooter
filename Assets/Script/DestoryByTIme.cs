e09646d89363cec363e024bd813b164a6263942c
Zerozhonglin <1197240598@qq.com>
2017-05-20 12:26:40 +0800

    First

---
+++ Assets/Script/DestoryByTIme.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using UnityEngine;
+using  System.Collections.Generic;  
+
+public class DestoryByTime : MonoBehaviour {
+	public float lifetime;
+	void Start(){
+		DestroyObject (gameObject, lifetime);
+	}
+}
